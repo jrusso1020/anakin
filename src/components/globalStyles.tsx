8020fc1fb157b3fa4d039c142ca4bba9150908bf
James Russo <jrusso@brex.com>
2023-07-18 12:25:14 -0400

    update formatting

--- src/components/globalStyles.tsx
+++ src/components/globalStyles.tsx
@@ -1,26 +1,28 @@
 import { Global, css } from "@emotion/react"
 
-const GlobalStyles = () => <Global
-  styles={css`
-  a {
-    color: #3AAFA9;
-    box-shadow: none;
-    text-decoration: none;
-  }
+const GlobalStyles = () => (
+  <Global
+    styles={css`
+      a {
+        color: #3aafa9;
+        box-shadow: none;
+        text-decoration: none;
+      }
 
-  a:hover {
-    box-shadow: 0 1px 0 0 #3AAFA9;
-  }
+      a:hover {
+        box-shadow: 0 1px 0 0 #3aafa9;
+      }
 
-  blockquote {
-    color: #FEFFFF;
-  }
+      blockquote {
+        color: #feffff;
+      }
 
-  .gatsby-resp-image-link:hover {
-    box-shadow: none;
-    text-decoration: none;
-  }
-`}
-/>
+      .gatsby-resp-image-link:hover {
+        box-shadow: none;
+        text-decoration: none;
+      }
+    `}
+  />
+)
 
 export default GlobalStyles
