8113b5edaea84ae3b9662864cb5d6d947622db2c
James Russo <jdrusso1020@gmail.com>
2024-06-30 22:31:03 -0400

    layout converted

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,39 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
-import styled from "@emotion/styled"
-import { css } from "@emotion/react"
 
 import Navbar from "./navbar"
 import H1 from "./H1"
-import { rhythm } from "src/utils/typography"
-import { mobile } from "src/utils/media"
-import { colors } from "src/utils/colors"
 
-const BodyContainer = styled.div`
-  margin-left: calc(320px - 1em);
-  margin-right: auto;
-  max-width: ${rhythm(28)};
-  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
-  ${mobile(css`
-    margin-left: 0;
-    margin-right: 0;
-  `)}
-`
-
-const StyledLink = styled(Link)`
-  box-shadow: none;
-  text-decoration: none;
-  color: ${(props) => (props.color ? props.color : colors.keppel)};
-`
-
-const H3 = styled.h3`
-  font-family: Montserrat, sans-serif;
-  margin-top: 0;
-`
-
-const Footer = styled.footer`
-  color: ${(props) => (props.color ? props.color : colors.mirage)};
-`
+const StyledLink = ({
+  children,
+  to,
+}: {
+  children: React.ReactNode
+  color?: string
+  to: string
+}) => (
+  <Link
+    className={`no-underline text-keppel shadow-none hover:shadow-keppel`}
+    to={to}
+  >
+    {children}
+  </Link>
+)
 
 interface Props {
   location: Location
@@ -53,29 +38,30 @@ const Layout = ({ location, title, children }: Props) => {
     )
   } else {
     header = (
-      <H3>
+      <h3 className="mt-0 text-xl mb-6">
         <StyledLink to={"/"}>{title}</StyledLink>
-      </H3>
+      </h3>
     )
   }
   return (
     <div>
       <Navbar location={location} />
-      <BodyContainer>
+      <div className="md:ml-80 md:mr-auto max-w-7xl p-6 mx-0">
         <header>{header}</header>
         <main>{children}</main>
-        <Footer>
+        <footer className="text-mirage">
           © {new Date().getFullYear()} James Russo. All Rights Reserved, Built
           with{" "}
           <a
+            className="text-keppel"
             href="https://www.gatsbyjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Gatsby
           </a>
-        </Footer>
-      </BodyContainer>
+        </footer>
+      </div>
     </div>
   )
 }
