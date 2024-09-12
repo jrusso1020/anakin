75ab83f7a8a5bb3064b23e8af6c9be0ffb72df35
James Russo <jdrusso1020@gmail.com>
2024-09-12 19:58:03 -0400

    try changes for offline and service worker to help with blank page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,14 @@ import "./src/styles/globals.css"
 import "typeface-montserrat"
 import "typeface-merriweather"
 require("prismjs/themes/prism-tomorrow.css")
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
